f131c980b4627a53fcaed6b58cb3068fde4a3060
rin-w <36298836+rin-w@users.noreply.github.com>
2023-09-30 16:22:30 +0000

    Add files via upload

--- src/.vitepress/theme/index.ts
+++ src/.vitepress/theme/index.ts
@@ -1,16 +1,38 @@
 // https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
 import Theme from 'vitepress/theme'
-import './style.css'
+import './style.less'
+
+import { onMounted, watch, nextTick } from 'vue';
+import { useRoute } from 'vitepress';
+import mediumZoom from 'medium-zoom';
 
 export default {
+
   extends: Theme,
+
   Layout: () => {
     return h(Theme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     })
   },
+
   enhanceApp({ app, router, siteData }) {
     // ...
-  }
+  },
+
+  setup() {
+    const route = useRoute();
+    const initZoom = () => {
+      mediumZoom('.main img', { background: 'var(--vp-c-bg)' });
+    };
+    onMounted(() => {
+      initZoom();
+    });
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    );
+  },
+
 }
